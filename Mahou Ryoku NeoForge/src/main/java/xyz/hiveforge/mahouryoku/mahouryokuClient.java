9f25ff3aa4e4fb2f71cf07fb9760ee688d3fd69a
MichaelWard405 <michaelgn405@gmail.com>
2026-04-29 17:59:26 +0000

    Cleaned MDK

--- Mahou Ryoku NeoForge/src/main/java/xyz/hiveforge/mahouryoku/mahouryokuClient.java
+++ Mahou Ryoku NeoForge/src/main/java/xyz/hiveforge/mahouryoku/mahouryokuClient.java
@@ -10,22 +10,12 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-// This class will not load on dedicated servers. Accessing client side code from here is safe.
 @Mod(value = mahouryoku.MODID, dist = Dist.CLIENT)
-// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
 @EventBusSubscriber(modid = mahouryoku.MODID, value = Dist.CLIENT)
 public class mahouryokuClient {
     public mahouryokuClient(ModContainer container) {
-        // Allows NeoForge to create a config screen for this mod's configs.
-        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
-        // Do not forget to add translations for your config options to the en_us.json file.
-        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
-
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
-        // Some client setup code
-        mahouryoku.LOGGER.info("HELLO FROM CLIENT SETUP");
-        mahouryoku.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
