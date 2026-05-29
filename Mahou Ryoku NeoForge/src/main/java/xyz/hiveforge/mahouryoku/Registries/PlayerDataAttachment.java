fa05d3f45ac972172fbdb0b18842a1635a3a6beb
MichaelWard405 <michaelgn405@gmail.com>
2026-05-29 14:56:32 +0000

    Updated Registeries to Player Data Attachment

--- Mahou Ryoku NeoForge/src/main/java/xyz/hiveforge/mahouryoku/Registries/PlayerDataAttachment.java
+++ Mahou Ryoku NeoForge/src/main/java/xyz/hiveforge/mahouryoku/Registries/PlayerDataAttachment.java
@@ -2,15 +2,15 @@ package xyz.hiveforge.mahouryoku.Registries;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.attachment.AttachmentType;
-import net.neoforged.neoforge.attachment.IAttachmentSerializer;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import xyz.hiveforge.mahouryoku.magic_systems.mahou.MahouPool;
+import xyz.hiveforge.mahouryoku.race_system.PlayerRaceData;
 
 import java.util.function.Supplier;
 
 
-public class MagicRegistries {
+public class PlayerDataAttachment {
     //Registers the Players Codec as a Capability
     public static final DeferredRegister<AttachmentType<?>> ATTACHMENT_TYPES =
             DeferredRegister.create(NeoForgeRegistries.Keys.ATTACHMENT_TYPES, "mahouryoku");
@@ -22,6 +22,16 @@ public class MagicRegistries {
             ATTACHMENT_TYPES.register("mahou_pool", () -> AttachmentType.<MahouPool>builder(MahouPool::new)
                     .serialize(MahouPool.MAP_CODEC)
                     //.copyOnDeath()
+                    .build());
+
+
+    //===============================
+    //     Player Race Capability
+    //===============================
+    public static final Supplier<AttachmentType<PlayerRaceData>> PLAYER_RACE =
+            ATTACHMENT_TYPES.register("player_race", () -> AttachmentType.builder(PlayerRaceData::new)
+                    .serialize(PlayerRaceData.MAP_CODEC)
+                    .copyOnDeath()
                     .build());
 
     public static void register(IEventBus modBus) {
