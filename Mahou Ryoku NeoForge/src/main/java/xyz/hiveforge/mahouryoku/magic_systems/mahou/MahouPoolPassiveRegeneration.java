9b3045a505957cca1ec6a8970beadf7f80e1be1b
MichaelWard405 <michaelgn405@gmail.com>
2026-05-29 14:58:30 +0000

    Clarified File

--- Mahou Ryoku NeoForge/src/main/java/xyz/hiveforge/mahouryoku/magic_systems/mahou/MahouPoolPassiveRegeneration.java
+++ Mahou Ryoku NeoForge/src/main/java/xyz/hiveforge/mahouryoku/magic_systems/mahou/MahouPoolPassiveRegeneration.java
@@ -5,7 +5,7 @@ import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
-import xyz.hiveforge.mahouryoku.Registries.MagicRegistries;
+import xyz.hiveforge.mahouryoku.Registries.PlayerDataAttachment;
 
 @EventBusSubscriber(modid = "mahouryoku")
 public class MahouPoolPassiveRegeneration {
@@ -18,13 +18,13 @@ public class MahouPoolPassiveRegeneration {
     @SubscribeEvent
     public static void onPlayerTick(PlayerTickEvent.Post event) {
         if (event.getEntity() instanceof ServerPlayer player) {
-            MahouPool data = player.getData(MagicRegistries.MAHOU_POOL.get());
+            MahouPool data = player.getData(PlayerDataAttachment.MAHOU_POOL.get());
             if (data.GetMahouPool){
                 data.RegenerateMahouPool(.03f);
 
             }
             if(player.tickCount % 20 == 0) {
-                player.setData(MagicRegistries.MAHOU_POOL.get(), data);
+                player.setData(PlayerDataAttachment.MAHOU_POOL.get(), data);
             }
         }
 
